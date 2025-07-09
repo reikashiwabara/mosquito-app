e3204372c13972ab80d742515d9514ddc26cb406
kervint1 <s1f102302124@iniad.org>
2025-07-09 17:34:51 +0900

    lockjson削除

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,11 @@
- 
+const express = require('express');
+const app = express();
+const port = 3001; // listenするport番号
+
+app.get('/test', (req: any, res: { send: (arg0: string) => void; }) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
