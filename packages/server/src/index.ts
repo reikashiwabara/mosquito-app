52ccc3dd5410dc327aad149b98869e440bc1121a
kervint1 <s1f102302124@iniad.org>
2025-07-10 08:36:40 +0900

    DB設定

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,9 @@ const port = 3001; // listenするport番号
 app.get('/test', (req: any, res: { send: (arg0: string) => void; }) => {
     res.send("hello world");
 });
+//routesディレクトリにある全てのルートを読み込む
+app.use('/api', require('./routes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
